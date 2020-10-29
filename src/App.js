37ecf6f867206e96a79efa80a89154704aca29e2
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-10-28 22:05:51 -0400

    test code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Formatter</h1>
+      <h1> This is a new version of the server - path - formatter </h1>
+      <p> here is a paragraph</p>
     </div>
   );
 }
